24db3db70d20b1203755b22b7800c4ccc6b9e73e
biezhi <biezhi.me@gmail.com>
2016-03-22 16:07:49 +0800

    -

--- src/test/java/com/junicorn/jmail/MailTest.java
+++ src/test/java/com/junicorn/jmail/MailTest.java
@@ -18,10 +18,10 @@ public class MailTest {
 			.subject("hi，您有一封注册邮件！！！")
 			.from("jelly_8090@163.com")
 			.content("<p>hello</p><a href='http://www.baidu.com'>world</a>")
-			.addTo("921293209@qq.com")
-			.addFile("/Users/Anne/Documents/3849072.jpg", "/Users/Anne/Documents/vps.md");
+			.addTo("921293209@qq.com")/*
+			.addFile("/Users/Anne/Documents/3849072.jpg", "/Users/Anne/Documents/vps.md")*/;
 			
-			mailSender.debug(true).host("smtp.163.com").username("jelly_8090@163.com").password("###");
+			mailSender.debug(true).host("smtp.163.com").username("jelly_8090@163.com").password("********");
 			mailSender.send(mailMessage);
 		} catch (MessagingException e) {
 			e.printStackTrace();
